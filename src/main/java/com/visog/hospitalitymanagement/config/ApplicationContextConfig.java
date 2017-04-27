960f321335906fced4520bf473d0bb5e790bbe13
K.REDDIPRASAD <K.REDDIPRASAD@192.168.0.8>
2017-04-27 09:09:40 +0530

    by DBTeam

---
+++ src/main/java/com/visog/hospitalitymanagement/config/ApplicationContextConfig.java
@@ -0,0 +1,34 @@
+package com.visog.hospitalitymanagement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * @author kmubarak
+ *
+ */
+@Configuration
+@ComponentScan("com.visog.hospitalitymanagement.*")
+public class ApplicationContextConfig {
+	
+	@Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource rb = new ResourceBundleMessageSource();
+        // Load property in message/validator.properties
+        rb.setBasenames(new String[] { "messages/validator" });
+        return rb;
+    }
+ 
+    @Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".html");
+        return viewResolver;
+    }
+     
+
+}
